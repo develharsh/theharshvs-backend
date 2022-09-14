a1db32401046049540b8a2a680f55ce88c3cd514
unknown <hvsreal.223@gmail.com>
2022-09-15 04:48:32 +0530

    asp

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -1,5 +1,5 @@
 const blogModel = require("../models/blog.model");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
 module.exports.add = async (req, res) => {
   try {
@@ -8,6 +8,32 @@ module.exports.add = async (req, res) => {
     res.status(201).json({
       success: true,
       message: "Posted Successfully",
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+module.exports.get = async (_, res) => {
+  try {
+    const blogs = await blogModel.find().sort({ createdAt: -1 });
+    res.status(200).json({
+      success: true,
+      message: "Fetched Blogs",
+      blogs,
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+module.exports.getBySlug = async (req, res) => {
+  try {
+    const blog = await blogModel.findOne({ slug: req.params.slug });
+    if (!blog) throw { message: "No Such Blog Found" };
+    res.status(200).json({
+      success: true,
+      message: "Fetched Blog",
+      blog,
     });
   } catch (error) {
     res.status(500).json({ success: false, message: error.message });
--- routes/blog.route.js
+++ routes/blog.route.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const blogController = require("../controllers/blog.controller");
 
 router.post("/add", blogController.add);
+router.get("/get", blogController.get);
+router.get("/get-by-slug/:slug", blogController.getBySlug);
 
 module.exports = router;
